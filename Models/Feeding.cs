21729c44ab50f36c45162225c255526372fc11d1
Tricia Amheiser <tricia@taarts.net>
2022-08-11 21:29:39 -0400

    Added migrations Feeding, Scolding, Playtime in "Scolding"

--- Models/Feeding.cs
+++ Models/Feeding.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Collections.Generic;
 
 namespace TamagotchiAPI.Models
 {
-    public class Playtime
+    public class Feeding
     {
         public int Id { get; set; }
         public DateTime When { get; set; }
         public int PetId { get; set; }
-
-        // public List<Pet> Pets { get; set; }
     }
 }
